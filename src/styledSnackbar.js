459e42d1435f928b7abda6cd9cd176bb8f7b3674
JeongSeob LEE <74814652+DenebLee@users.noreply.github.com>
2021-09-01 12:55:29 +0900

    Update styledSnackbar.js

--- src/styledSnackbar.js
+++ src/styledSnackbar.js
@@ -12,3 +12,6 @@ const SnackbarStyled = styled.div`
 export const Snackbar = ({ message, ...props }) => {
   return <SnackbarStyled>{message}</SnackbarStyled>;
 };
+
+//snackbar를 굳이 사용안해도 되는 경우는 alert창을 커스텀할수 있을경우 혹은 다른 방식(modal)로 alert를 주는경우 
+//해당 컴포넌트에서 style지정을 통해 사용자가 유지보수하기 
